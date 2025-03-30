952a1057bb171c9369193a294a091211dea6fdd9
Jae <jaeyeonjung3@gmail.com>
2025-03-30 23:57:44 +0900

    [Refactor] 테스트 오류: 컴포넌트를 정규화한다 - 타입이 함수인 경우 props만 포함에서, children도 포함되도록 수정 - children이 null인 경우 방어 추가

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -9,13 +9,28 @@ export function normalizeVNode(vNode) {
   }
   // vNode의 타입이 함수일 경우 해당 함수를 호출하여 반환된 결과를 재귀적으로 표준화합니다.
   if (typeof vNode.type === "function") {
-    return normalizeVNode(vNode.type(vNode.props));
+    const props = { ...(vNode.props || {}), children: vNode.children };
+    return normalizeVNode(vNode.type(props));
+  }
+
+  // vNode.children이 배열이 아닐 경우 배열로 변환
+  let children = []; // vNode.children이 없을 경우 방지
+  if (vNode.children) {
+    children = Array.isArray(vNode.children)
+      ? vNode.children
+      : [vNode.children];
   }
 
   // 그 외의 경우, vNode의 자식 요소들을 재귀적으로 표준화하고, null 또는 undefined 값을 필터링하여 반환합니다.
-  const children = (vNode.children || [])
+  const normalizedChildren = children
     .map((child) => normalizeVNode(child))
-    .filter((child) => child != null); // null 또는 undefined 값을 필터링
+    .filter(
+      (child) =>
+        child !== "" &&
+        child !== null &&
+        child !== undefined &&
+        child !== false,
+    ); // Falsy 값 (null, undefined, false, 빈 문자열 "")을 필터링
 
-  return { ...vNode, children };
+  return { ...vNode, children: normalizedChildren };
 }
