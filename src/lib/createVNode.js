1b692d2872bfd00d250a259e8b02c8173523aa23
Jae <jaeyeonjung3@gmail.com>
2025-03-30 22:22:23 +0900

    [Feat] createVNode 함수 작성

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,6 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  const filteredChildren = children
+    .flat(Infinity) // 자식 노드가 배열일 경우 평탄화 (제일 깊은 객체가 Infinity이므로)
+    .filter((child) => child != null && child !== false);
+  return { type, props, children: filteredChildren };
 }
