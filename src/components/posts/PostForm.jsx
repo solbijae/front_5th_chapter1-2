0d80741f9e743735261406c2c16ce7a0c39a545f
Jae <jaeyeonjung3@gmail.com>
2025-03-31 22:01:04 +0900

    [Feat] 사용자: 포스트를 추가할 수 있다.

--- src/components/posts/PostForm.jsx
+++ src/components/posts/PostForm.jsx
@@ -1,7 +1,24 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
+import { globalStore } from "../../stores";
+const postForm = (content) => {
+  const { currentUser, posts } = globalStore.getState();
+  const post = {
+    id: posts.length + 1,
+    author: currentUser.username,
+    content: content,
+    likeUsers: [],
+    time: Date.now()
+  }
+  globalStore.setState({ posts: [...posts, post]})
+}
 
 export const PostForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const content = document.getElementById("post-content").value;
+    postForm(content);
+  }
   return (
     <div className="mb-4 bg-white rounded-lg shadow p-4">
       <textarea
@@ -12,6 +29,7 @@ export const PostForm = () => {
       <button
         id="post-submit"
         className="mt-2 bg-blue-600 text-white px-4 py-2 rounded"
+        onClick={handleSubmit}
       >
         게시
       </button>
