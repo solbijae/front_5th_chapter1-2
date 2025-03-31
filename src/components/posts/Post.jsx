fd571fa70b0fef64ccc23788a78905c5388617ac
Jae <jaeyeonjung3@gmail.com>
2025-03-31 21:45:38 +0900

    [Feat] 사용자: 포스트에 좋아요를 클릭할 경우, 좋아요가 토글된다.

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -3,24 +3,40 @@ import { createVNode } from "../../lib";
 import { globalStore } from "../../stores";
 import { toTimeFormat } from "../../utils/index.js";
 
-function like() {
-  const { loggedIn } = globalStore.getState();
+function like(id) {
+  const { loggedIn, currentUser, posts } = globalStore.getState();
   if (!loggedIn) {
     alert("로그인 후 이용해주세요");
     return;
   }
+
+  const post = posts.find((post) => post.id === id);
+  if (post.likeUsers.includes(currentUser.username)) {
+    post.likeUsers = post.likeUsers.filter(
+      (user) => user !== currentUser.username,
+    );
+  } else {
+    post.likeUsers.push(currentUser.username);
+    post.activationLike = true;
+  }
+  globalStore.setState({ posts });
+
+  console.log(document.querySelectorAll("#posts-container .like-button")[0].outerHTML)
 }
 
 export const Post = ({
+  id,
   author,
   time,
   content,
   likeUsers,
   activationLike = false,
 }) => {
+  const { currentUser } = globalStore.getState();
+  const isLiked = currentUser && likeUsers.includes(currentUser.username);
   const handleLike = (e) => {
     e.preventDefault();
-    like();
+    like(id);
   };
   return (
     <div className="bg-white rounded-lg shadow p-4 mb-4">
@@ -33,7 +49,7 @@ export const Post = ({
       <p>{content}</p>
       <div className="mt-2 flex justify-between text-gray-500">
         <span
-          className={`like-button cursor-pointer${activationLike ? " text-blue-500" : ""}`}
+          className={`like-button cursor-pointer${isLiked ? " text-blue-500" : ""}`}
           onClick={handleLike}
         >
           좋아요 {likeUsers.length}
