7352ca732414e8552c80f1cf9ffab868e3f774e2
Jae <jaeyeonjung3@gmail.com>
2025-03-31 21:13:50 +0900

    [Feat] 비사용자: 포스트에서 좋아요를 누를 경우 "로그인 후 이용해주세요" alert가 보여진다.

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,6 +1,15 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
+import { globalStore } from "../../stores";
 import { toTimeFormat } from "../../utils/index.js";
+
+function like() {
+  const { loggedIn } = globalStore.getState();
+  if (!loggedIn) {
+    alert("로그인 후 이용해주세요");
+    return;
+  }
+}
 
 export const Post = ({
   author,
@@ -9,6 +18,10 @@ export const Post = ({
   likeUsers,
   activationLike = false,
 }) => {
+  const handleLike = (e) => {
+    e.preventDefault();
+    like();
+  };
   return (
     <div className="bg-white rounded-lg shadow p-4 mb-4">
       <div className="flex items-center mb-2">
@@ -21,6 +34,7 @@ export const Post = ({
       <div className="mt-2 flex justify-between text-gray-500">
         <span
           className={`like-button cursor-pointer${activationLike ? " text-blue-500" : ""}`}
+          onClick={handleLike}
         >
           좋아요 {likeUsers.length}
         </span>
