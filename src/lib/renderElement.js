682b7d00bf758c432f5ca879ffc9abf7a2ee3695
Jae <jaeyeonjung3@gmail.com>
2025-03-31 20:49:42 +0900

    [Feat] 초기 렌더링이 아닐 경우 updateElement 사용

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,22 +1,26 @@
 import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
-// import { updateElement } from "./updateElement";
+import { updateElement } from "./updateElement";
 
+let previousVNode = null; // 이전 가상 DOM 트리 저장
 export function renderElement(vNode, container) {
   // vNode 정규화
   const normalizedVNode = normalizeVNode(vNode);
 
-  // createElement로 노드를 만들고
-  const element = createElement(normalizedVNode);
+  // 최초 렌더링인지 확인 (#root가 비어있는지 확인)
+  const isInitialRender = container.childNodes.length === 0;
 
-  // container에 삽입하고
-  container.replaceChildren(element);
+  if (isInitialRender) {
+    // 최초 렌더링시에는 createElement로 DOM을 생성하고
+    const element = createElement(normalizedVNode);
+    container.replaceChildren(element);
+  } else {
+    // 이후에는 updateElement로 기존 DOM을 업데이트한다.
+    updateElement(container, normalizedVNode, previousVNode);
+  }
 
-  // 이벤트를 등록합니다.
-  setupEventListeners(container);
-
-  // 최초 렌더링시에는 createElement로 DOM을 생성하고
-  // 이후에는 updateElement로 기존 DOM을 업데이트한다.
+  previousVNode = normalizedVNode;
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
+  setupEventListeners(container);
 }
